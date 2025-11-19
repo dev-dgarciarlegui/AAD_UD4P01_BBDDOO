c372282dd19801250c3f2a014cab22a5e3ffe678
Daniel Garcia <daniel.garcia.arlegui@gmail.com>
2025-11-19 10:30:19 +0100

    creados métodos de la clase instituto

--- src/main/java/es/ciudadescolar/instituto/Instituto.java
+++ src/main/java/es/ciudadescolar/instituto/Instituto.java
@@ -3,14 +3,18 @@ package es.ciudadescolar.instituto;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Instituto {
+
+    private static Logger LOG = LoggerFactory.getLogger("Instituto.class");
 
     private String nombre;
     private Integer id;
-    List<Alumno> listaAlumnos;
+    List<Alumno> listaAlumnos = null;
     
     public Instituto() {
-        this.listaAlumnos = new ArrayList<>();
     }
 
     public Instituto(String nombre, Integer id, List<Alumno> listaAlumnos) {
@@ -46,6 +50,47 @@ public class Instituto {
     @Override
     public String toString() {
         return "Instituto [nombre=" + nombre + ", id=" + id + ", listaAlumnos=" + listaAlumnos + "]";
+    }
+
+    public void matricularAlumno(Alumno al) {
+        if(this.listaAlumnos == null) {
+            this.listaAlumnos = new ArrayList<>();            
+        }
+        listaAlumnos.add(al);
+        LOG.info("Alumno añadido a la lista de alumnos: "+al);
+    }
+
+    public void expulsarAlumno(Alumno al) {
+        if (this.listaAlumnos == null) {
+            this.listaAlumnos = new ArrayList<>();
+        }
+        if (listaAlumnos.size() == 0) {
+            LOG.warn("No se puede expulsar al alumno porque la lista está vacía: "+al);
+        }
+        else {
+            if(listaAlumnos.contains(al)) {
+                listaAlumnos.remove(al);
+                LOG.info("Alumno eliminado de la lista de alumnos: "+al);
+            }
+            else {
+                LOG.warn("El alumnos no se encuentra en la lista de alumnos: "+al);
+            }
+        }
+    }
+
+    public boolean cambiarIdInstituto(int nuevoId) {
+        boolean status = true;
+
+        if (nuevoId != this.id) {
+            this.setId(nuevoId);
+            LOG.info("Se ha cambiado el id del instituto: "+id);
+        }
+        else {
+            LOG.warn("No se ha cambiado el id del instituto porque es igual que el id que ya tenía: "+id);
+            status = false;
+        }
+
+        return status;
     }
 
 }
---
+++ src/main/java/es/ciudadescolar/utils/Bbddoo.java
@@ -0,0 +1,7 @@
+package es.ciudadescolar.utils;
+
+public class Bbddoo {
+
+    
+
+}
